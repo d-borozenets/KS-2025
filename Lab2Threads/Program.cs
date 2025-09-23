f305e3eb9b49bc8d781cce1899e39951503e0ba1
Dmytro <dimkabv@gmail.com>
2025-09-23 21:01:00 +0300

    calc fix

--- Lab2Threads/Program.cs
+++ Lab2Threads/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void HeavyCalculations(int index)
     {
-        
+
         double x = 6464.3232;
 
         const int count = 10000;
@@ -14,14 +14,15 @@ class Program
             for (int j = 0; j < count; j++)
             {
                 x = Math.Pow(x, 2);
-                x *= 0.5;
+                x += 0.00000000001;
                 x = Math.Sqrt(x);
             }
         }
+        Console.WriteLine($"Complete[{index}] x = {x}");
     }
-    
+
     static void Main()
     {
         Console.WriteLine($"Hello World");
-    }    
+    }
 }
