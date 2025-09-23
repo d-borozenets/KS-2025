796256e308a336df29ffd7611b48459f4e0a2c64
Dmytro <dimkabv@gmail.com>
2025-09-23 20:26:08 +0300

    Start

---
+++ Lab2Threads/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void HeavyCalculations(int index)
+    {
+        
+        double x = 6464.3232;
+
+        const int count = 10000;
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < count; j++)
+            {
+                x = Math.Pow(x, 2);
+                x *= 0.5;
+                x = Math.Sqrt(x);
+            }
+        }
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine($"Hello World");
+    }    
+}
