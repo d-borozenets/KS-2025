c8f2817030ffff5f747c7524f35353c52cb52e64
Dmytro <dimkabv@gmail.com>
2025-10-12 21:51:11 +0300

    lecture 5

---
+++ Lecture5/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    // static async Task Main(){
+        // await RunAwait();
+        // await RunContinue();
+    // }
+    static void Main(){
+        ParallelFor();
+        // Task t = RunContinue();
+        // t.Wait();
+    }
+
+    private static void ParallelFor(){
+        Parallel.For(0, 10, i => {
+            Console.WriteLine($"item {i}, thread: {Thread.CurrentThread.ManagedThreadId}");
+        });
+    }
+
+    private static Task RunContinue(){
+        return Task.Run(() => Console.WriteLine("Hello async"))
+            .ContinueWith(t => Console.WriteLine("Продовження після завершення"));
+    }
+
+    private static async Task RunAwait(){
+        await Task.Run(() => Console.WriteLine("Hello async"));
+        Console.WriteLine("Продовження після завершення");
+    }
+}
