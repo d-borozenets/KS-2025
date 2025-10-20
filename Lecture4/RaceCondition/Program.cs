9849f13dc1ccd83abb4d155583acf37cc9f53147
Dmytro <dimkabv@gmail.com>
2025-10-20 21:47:13 +0300

    l4. race condintion

---
+++ Lecture4/RaceCondition/Program.cs
@@ -0,0 +1,40 @@
+﻿// File: Program.cs
+using System;
+using System.Threading;
+
+class Program
+{
+    static int counter = 0;                 // shared resource (intentionally unprotected)
+    const int THREADS = 8;
+    const int ITERATIONS_PER_THREAD = 1_000_000;
+    static readonly object locker = new object();
+
+    static void Worker()
+    {
+        for (int i = 0; i < ITERATIONS_PER_THREAD; i++)
+        {
+            // Non-atomic operation: read -> modify -> write
+            // lock (locker) 
+            {
+                counter++;   
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Thread[] threads = new Thread[THREADS];
+
+        for (int i = 0; i < THREADS; i++)
+        {
+            threads[i] = new Thread(Worker);
+            threads[i].Start();
+        }
+
+        foreach (Thread t in threads)
+            t.Join();
+
+        Console.WriteLine($"Expected: {THREADS * ITERATIONS_PER_THREAD}");
+        Console.WriteLine($"Actual:   {counter}");
+    }
+}
