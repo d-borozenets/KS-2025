0f9caf8e4467051962408caddc1b21a99068fd47
Dmytro <dimkabv@gmail.com>
2025-11-02 13:00:30 +0200

    lab 8 added

---
+++ Lab8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program{
+    static int counter;
+    const int N = 200_000_000;
+    // const int N = 100_000; // use for quick debugging
+
+    static void Main(){
+        Console.WriteLine("=== Lab 8: Basic Synchronization ===\n");
+        RunTest("1. No synchronization", IncrementUnsafe);
+        RunTest("2. lock", IncrementLock);
+        RunTest("3. Monitor.Enter/Exit", IncrementMonitor);
+        RunTest("4. Interlocked", IncrementAtomic);
+    }
+
+    static void RunTest(string title, ThreadStart method){
+    }
+
+    static void IncrementUnsafe(){
+    }
+
+    static void IncrementLock(){
+    }
+
+    static void IncrementMonitor(){
+    }
+
+    static void IncrementAtomic(){
+    }
+}
