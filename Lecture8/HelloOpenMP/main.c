5d0f3bce6da71fda08ab0a6eeb340a1f0138cd0d
Dmytro <dimkabv@gmail.com>
2025-11-13 20:17:14 +0200

    open mp

--- Lecture8/HelloOpenMP/main.c
+++ Lecture8/HelloOpenMP/main.c
@@ -2,25 +2,25 @@
 #include <omp.h>
 #include <stdlib.h>
 
-int main() {
-
-#pragma omp parallel
-    {
-        int id = omp_get_thread_num();
-        int n = omp_get_num_threads();
-        printf("Hello from thread %d of %d\n", id, n);
-    }
-    return 0;
-}
+// int main() {
+// #pragma omp parallel
+//     {
+//         int id = omp_get_thread_num();
+//         int n = omp_get_num_threads();
+//         printf("Hello from thread %d of %d\n", id, n);
+//     }
+//     return 0;
+// }
 
 //----------------3.2-----------------
 // int main() {
 //     omp_set_num_threads(4);
-//     // int i;
+//     int i;
 // #pragma omp parallel for
 //     for (int i = 0; i < 8; i++) {
 //         int id = omp_get_thread_num();
-//         printf("i = %d виконано потоком %d\n", i, id);
+//         // int id = i;
+//         printf("i = %d be thread %d\n", i, id);
 //     }
 //     return 0;
 // }
@@ -53,19 +53,27 @@ int main() {
 //     const int N = 16;
 //     double start, end;
 //     omp_set_num_threads(4);
+//
+//     int owner[N];
 //     start = omp_get_wtime();
 //
-// #pragma omp parallel for schedule(static, 4)
-// //#pragma omp parallel for schedule(dynamic, 4)
-// //#pragma omp parallel for schedule(guided, 4)
+// // #pragma omp parallel for schedule(static, 4)
+// #pragma omp parallel for schedule(dynamic, 4)
+// // #pragma omp parallel for schedule(guided, 4)
 //     for (int i = 0; i < N; i++) {
 //         int t = omp_get_thread_num();
-//         printf("Iteration %2d executed by thread %d\n", i, t);
-//         for (volatile long j = 0; j < 10000000; j++); // simulate workload
+//         // for (volatile long j = 0; j < 10000000; j++); // simulate workload
+//         for (volatile long j = 0; j < (i + 1) * 500000000; j++);
+//         printf("Iteration %2d job done by thread %d\n", i, t);
+//         owner[i] = omp_get_thread_num();
 //     }
 //
 //     end = omp_get_wtime();
 //     printf("Execution time: %.3f sec\n", end - start);
+//     // Print sequentially to show the mapping clearly
+//     for (int i = 0; i < N; i++) {
+//         printf("i=%2d handled by thread %d\n", i, owner[i]);
+//     }
 //     return 0;
 // }
 //----------------5.2-----------------
@@ -87,19 +95,24 @@ int main() {
 //----------------6.4-----------------
 // int main() {
 //     putenv("OMP_CANCELLATION=true");
+//     omp_set_num_threads(4);
 // #pragma omp parallel for
-//     for (int i = 0; i < 10; i++) {
-//         if (i == 5) {
+//     for (int i = 0; i < 20; i++) {
+//         printf("Thread %d processing i=%d\n", omp_get_thread_num(), i);
+//         if (i == 1) {
 //             printf("Thread %d: condition met, cancelling loop\n",
 //                    omp_get_thread_num());
 // #pragma omp cancel for
 //         }
-//         printf("Thread %d processing i=%d\n",
-//                omp_get_thread_num(), i);
+//         // Кожен потік перевіряє, чи не треба зупинитись
+// #pragma omp cancellation point for
+//         printf("Thread %d processing i=%d\n", omp_get_thread_num(), i);
+//         for (volatile long j = 0; j < 100000000; j++);
 //     }
 //     return 0;
 // }
 
+//export OMP_NUM_THREADS=4; ./hello_omp
 
 //----------------7.4-----------------
 // #define N 100000000
